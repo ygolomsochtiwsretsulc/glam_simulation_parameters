a38749ef3b1655ba8ce2453b626bfa7f9922eee0
JohanComparat <johan.comparat@gmail.com>
2020-07-10 10:57:16 +0200

    example parameters

--- example_grid_w_pydoe/glam_lhs.py
+++ example_grid_w_pydoe/glam_lhs.py
@@ -1,17 +1,17 @@
 from pyDOE import *
 import numpy as n
+
 # set of parameters
-# example with the mira titan ones, with Omega_nu=0, Omega_K=0
-omega_m = [0.12, 0.155] # need to be exteneded to [0.08, 0.17]
-omega_b = [0.0215, 0.0235]
-sigma_8 = [0.7, 0.9] # need to extend to [0.6, 1.0]
-h       = [0.55, 0.85] # could be shrinked ?
+omega_m = [0.08, 0.17] 
+omega_b = [0.021, 0.024]
+sigma_8 = [0.6, 1.0] 
+h       = [0.6, 0.8] 
 ns      = [0.85, 1.05]
-w0      = [-1.3, -0.7] # [-1.5, -0.5] ?
-wa      = [-1.5, 1.15] # 
+w0      = [-1.5, -0.5] 
+#wa      = [-1.5, 1.15] # 
 
 # generate the parameters between 0 and 1
-n_parameters = 7
+n_parameters = 6
 n_cosmologies = 100 
 
 # criterion='maximin': maximize the minimum distance between points, but place the point in a randomized location within its interval
@@ -24,8 +24,8 @@ list_sigma_8 = grid_maximin.T[2] * ( sigma_8[1] - sigma_8[0] ) + sigma_8[0]
 list_h       = grid_maximin.T[3] * ( h      [1] - h      [0] ) + h      [0]
 list_ns      = grid_maximin.T[4] * ( ns     [1] - ns     [0] ) + ns     [0]
 list_w0      = grid_maximin.T[5] * ( w0     [1] - w0     [0] ) + w0     [0]
-list_wa      = grid_maximin.T[6] * ( wa     [1] - wa     [0] ) + wa     [0]
+#list_wa      = grid_maximin.T[6] * ( wa     [1] - wa     [0] ) + wa     [0]
 
 # save the list
-DATA = n.transpose([list_omega_m, list_omega_b, list_sigma_8, list_h, list_ns, list_w0, list_wa])
-n.savetxt('cosmo_params.ascii', DATA, header = 'omega_m omega_b sigma_8 h ns w0 wa')
+DATA = n.transpose([list_omega_m, list_omega_b, list_sigma_8, list_h, list_ns, list_w0])
+n.savetxt('cosmo_params.ascii', DATA, header = 'omega_m omega_b sigma_8 h ns w0 ')
